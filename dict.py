283a2460027a4871d69d53ffacd5899e8e55c538
Marc <trem2842@usherbrooke.ca>
2020-03-05 19:55:03 -0500

    dictionaire popire

--- dict.py
+++ dict.py
@@ -1,18 +1,40 @@
-Dict = {'key': 'value'}
+Dict = {}
+i = 0
 
 with open('test.txt','r') as f:
     for line in f:
         for word in line.split():
-            Dict['n'] = word
+            Dict[i] = word
+            i = i + 1
 f.close()
 
+def wordListToFreqDict(wordlist):
+    wordfreq = [wordlist.count(p) for p in wordlist]
+    return dict(list(zip(wordlist,wordfreq)))
 
-# Creating an empty Dictionary 
-#Dict = {} 
-print("Empty Dictionary: ") 
+def sortFreqDict(freqdict):
+    aux = [(freqdict[key], key) for key in freqdict]
+    aux.sort()
+    aux.reverse()
+    return aux
+
+print("Dictionnaire : ") 
 print(Dict) 
-  
-#i = 1
-#Dict = dict(mots = word, frequence = i) 
-#print("\nDictionary with the use of dict(): ") 
-#print(Dict) 
+#print(Dict[1])
+#print(Dict.keys())
+
+
+fp = open('test.txt')
+data = fp.read()
+words = data.split()
+fp.close()
+
+unwanted_chars = ".,-_ (and so on)"
+wordfreq = {}
+for raw_word in words:
+    word = raw_word.strip(unwanted_chars)
+    if word not in wordfreq:
+        wordfreq[word] = 0 
+    wordfreq[word] += 1
+
+print(wordfreq)
